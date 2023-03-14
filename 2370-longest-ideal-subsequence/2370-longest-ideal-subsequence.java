51d2589518275e6b923c565f6dd33b9d15458a12
aruncse934 <46198451+aruncse934@users.noreply.github.com>
2023-03-15 00:40:29 +0530

    Time: 30 ms (66.36%), Space: 43.3 MB (36.36%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.java
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.java
@@ -1,19 +1,18 @@
 class Solution {
     public int longestIdealString(String s, int k) {
-         int[] maxEndingWith=new int[26];
-        int[] dp=new int[100006];
-        int ans=0;
-        for(int i = 1; i <= s.length(); i++) {
-            int cur = s.charAt(i-1) - 'a';
-            for(int j = cur-1; j >= Math.max(0, cur - k); j--) {
-                dp[i] = Math.max(dp[i], 1 + maxEndingWith[j]);
-            }
-            for(int j = cur; j <= Math.min(25, cur + k); j++) {
-                dp[i] = Math.max(dp[i], 1 + maxEndingWith[j]);
-            }
-            maxEndingWith[cur] = dp[i];
-            ans = Math.max(ans, dp[i]);
+        int DP[] = new int[26], ans = 1;
+
+        for (int ch = 0, n = s.length(); ch < n; ch++) {
+            int i = s.charAt(ch) - 'a';
+            DP[i] = DP[i] + 1;
+
+            for (int j = Math.max(0, i - k); j <= Math.min(25, i + k); j++)
+                if (j != i)
+                    DP[i] = Math.max(DP[i], DP[j] + 1);
+
+            ans = Math.max(ans, DP[i]);
         }
+
         return ans;
     }
 }
